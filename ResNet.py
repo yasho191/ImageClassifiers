daf540ae5340072b5424b00c3ecaf56fc817320a
Yashowardhan <yashowardhanshinde@gmail.com>
2022-01-18 00:03:17 +0530

    Added ResNet.py

---
+++ ResNet.py
@@ -0,0 +1,81 @@
+import tensorflow as tf
+
+def identity_block(x, filter):
+    # copy tensor to variable called x_skip
+    x_skip = x
+    
+    # Layer 1
+    x = tf.keras.layers.Conv2D(filter, (3,3), padding = 'same')(x)
+    x = tf.keras.layers.BatchNormalization(axis=3)(x)
+    x = tf.keras.layers.Activation('relu')(x)
+    
+    # Layer 2
+    x = tf.keras.layers.Conv2D(filter, (3,3), padding = 'same')(x)
+    x = tf.keras.layers.BatchNormalization(axis=3)(x)
+    
+    # Add Residue
+    x = tf.keras.layers.Add()([x, x_skip])     
+    x = tf.keras.layers.Activation('relu')(x)
+    
+    return x
+
+def convolutional_block(x, filter):
+    # copy tensor to variable called x_skip
+    x_skip = x
+    
+    # Layer 1
+    x = tf.keras.layers.Conv2D(filter, (3,3), padding = 'same', strides = (2,2))(x)
+    x = tf.keras.layers.BatchNormalization(axis=3)(x)
+    x = tf.keras.layers.Activation('relu')(x)
+    
+    # Layer 2
+    x = tf.keras.layers.Conv2D(filter, (3,3), padding = 'same')(x)
+    x = tf.keras.layers.BatchNormalization(axis=3)(x)
+    
+    # Processing Residue with conv(1,1)
+    x_skip = tf.keras.layers.Conv2D(filter, (1,1), strides = (2,2))(x_skip)
+    
+    # Add Residue
+    x = tf.keras.layers.Add()([x, x_skip])     
+    x = tf.keras.layers.Activation('relu')(x)
+    
+    return x
+
+def ResNet34(shape, classes):
+    # Step 1 (Setup Input Layer)
+    x_input = tf.keras.layers.Input(shape)
+    x = tf.keras.layers.ZeroPadding2D((3, 3))(x_input)
+    
+    # Step 2 (Initial Conv layer along with maxPool)
+    x = tf.keras.layers.Conv2D(64, kernel_size=7, strides=2, padding='same')(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation('relu')(x)
+    x = tf.keras.layers.MaxPool2D(pool_size=3, strides=2, padding='same')(x)
+    
+    # Define size of sub-blocks and initial filter size
+    block_layers = [3, 4, 6, 3]
+    filter_size = 64
+    
+    # Step 3 Add the Resnet Blocks
+    for i in range(4):
+        if i == 0:
+            # For sub-block 1 Residual/Convolutional block not needed
+            for j in range(block_layers[i]):
+                x = identity_block(x, filter_size)
+        else:
+            # One Residual/Convolutional Block followed by Identity blocks
+            # The filter size will go on increasing by a factor of 2
+            filter_size = filter_size*2
+            x = convolutional_block(x, filter_size)
+            for j in range(block_layers[i] - 1):
+                x = identity_block(x, filter_size)
+          
+    # Step 4 End Dense Network
+    x = tf.keras.layers.AveragePooling2D((2,2), padding = 'same')(x)
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Dense(512, activation = 'relu')(x)
+    x = tf.keras.layers.Dense(256, activation = 'relu')(x)
+    x = tf.keras.layers.Dense(classes, activation = 'softmax')(x)
+    
+    model = tf.keras.models.Model(inputs = x_input, outputs = x, name = "ResNet34")
+    return model
